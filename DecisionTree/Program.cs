7ad96ba00e11dcd3d4d5bb9df340ea91d1eeaa1f
Richard Bos <richard.bos93@gmail.com>
2018-05-17 15:44:22 +0200

    Changed Decision Tree Decisions (added a console table for display)

--- DecisionTree/Program.cs
+++ DecisionTree/Program.cs
@@ -4,6 +4,7 @@ using Accord.MachineLearning.DecisionTrees;
 using Accord.MachineLearning.DecisionTrees.Learning;
 using Accord.Math;
 using Accord.Statistics.Filters;
+using ConsoleTables;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -16,11 +17,13 @@ namespace DecisionTree
 {
     class Program
     {
+        static Codification Codification { get; set; }
+
         static void Main(string[] args)
         {
             // Create a new reader, opening a given path
-            ExcelReader excel = new ExcelReader("Intake Inf Cohort 2017.xlsx");
-            ExcelReader excelTest = new ExcelReader("Intake Inf Cohort 2017 - Test Case.xlsx");
+            ExcelReader excel = new ExcelReader("Intake Inf Cohort 2017 - Training Set.xlsx");
+            ExcelReader excelTest = new ExcelReader("Intake Inf Cohort 2017 - Test Set.xlsx");
 
             // Afterwards, we can query the file for all
             // worksheets within the specified workbook:
@@ -42,6 +45,9 @@ namespace DecisionTree
             // convert strings into integer symbols
             Codification codibook = new Codification(data);
 
+            // Set codibook
+            Codification = codibook;
+
             // Translate our training data into integer symbols using our codebook:
             DataTable symbols = codibook.Apply(data);
             int[][] inputs = symbols.ToJagged<int>(
@@ -54,26 +60,10 @@ namespace DecisionTree
                 codibook.Columns[14].ColumnName,
                 codibook.Columns[15].ColumnName,
                 codibook.Columns[16].ColumnName,
-                codibook.Columns[17].ColumnName,
-                codibook.Columns[18].ColumnName,
-                codibook.Columns[19].ColumnName,
                 codibook.Columns[20].ColumnName,
-                codibook.Columns[21].ColumnName,
-                codibook.Columns[22].ColumnName,
-                codibook.Columns[23].ColumnName,
-                codibook.Columns[24].ColumnName,
-                codibook.Columns[25].ColumnName,
-                codibook.Columns[26].ColumnName,
-                codibook.Columns[27].ColumnName,
-                codibook.Columns[28].ColumnName,
                 codibook.Columns[29].ColumnName,
                 codibook.Columns[30].ColumnName,
-                codibook.Columns[31].ColumnName,
-                codibook.Columns[32].ColumnName,
-                codibook.Columns[33].ColumnName,
-                codibook.Columns[34].ColumnName,
-                codibook.Columns[35].ColumnName,
-                codibook.Columns[36].ColumnName
+                codibook.Columns[34].ColumnName
             );
             int[] outputs = symbols.ToMatrix<int>(codibook.Columns[6].ColumnName).GetColumn(0);
 
@@ -89,34 +79,22 @@ namespace DecisionTree
                 new DecisionVariable(codibook.Columns[14].ColumnName, 10),
                 new DecisionVariable(codibook.Columns[15].ColumnName, 10),
                 new DecisionVariable(codibook.Columns[16].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[17].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[18].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[19].ColumnName, 2),
                 new DecisionVariable(codibook.Columns[20].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[21].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[22].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[23].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[24].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[25].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[26].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[27].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[28].ColumnName, 2),
                 new DecisionVariable(codibook.Columns[29].ColumnName, 2),
                 new DecisionVariable(codibook.Columns[30].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[31].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[32].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[33].ColumnName, 2),
                 new DecisionVariable(codibook.Columns[34].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[35].ColumnName, 2),
-                new DecisionVariable(codibook.Columns[36].ColumnName, 2),
             };
 
             // Learn the training instances!
             Accord.MachineLearning.DecisionTrees.DecisionTree tree = id3.Learn(inputs, outputs);
 
+            // Create a console table for display
+            ConsoleTable table = new ConsoleTable("Studentnumber", "Advice", "Conclusion");
+
             // Loop through each row in data
             foreach (DataRow row in dataTest.Rows)
             {
+
                 // The tree can now be queried for new examples through 
                 // its decide method. For example, we can create a query
                 int[] query = null;
@@ -134,30 +112,20 @@ namespace DecisionTree
                         { codibook.Columns[14].ColumnName, row.ItemArray[14].ToString() },
                         { codibook.Columns[15].ColumnName, row.ItemArray[15].ToString() },
                         { codibook.Columns[16].ColumnName, row.ItemArray[16].ToString() },
-                        { codibook.Columns[17].ColumnName, row.ItemArray[17].ToString() },
-                        { codibook.Columns[18].ColumnName, row.ItemArray[18].ToString() },
-                        { codibook.Columns[19].ColumnName, row.ItemArray[19].ToString() },
                         { codibook.Columns[20].ColumnName, row.ItemArray[20].ToString() },
-                        { codibook.Columns[21].ColumnName, row.ItemArray[21].ToString() },
-                        { codibook.Columns[22].ColumnName, row.ItemArray[22].ToString() },
-                        { codibook.Columns[23].ColumnName, row.ItemArray[23].ToString() },
-                        { codibook.Columns[24].ColumnName, row.ItemArray[24].ToString() },
-                        { codibook.Columns[25].ColumnName, row.ItemArray[25].ToString() },
-                        { codibook.Columns[26].ColumnName, row.ItemArray[26].ToString() },
-                        { codibook.Columns[27].ColumnName, row.ItemArray[27].ToString() },
-                        { codibook.Columns[28].ColumnName, row.ItemArray[28].ToString() },
                         { codibook.Columns[29].ColumnName, row.ItemArray[29].ToString() },
                         { codibook.Columns[30].ColumnName, row.ItemArray[30].ToString() },
-                        { codibook.Columns[31].ColumnName, row.ItemArray[31].ToString() },
-                        { codibook.Columns[32].ColumnName, row.ItemArray[32].ToString() },
-                        { codibook.Columns[33].ColumnName, row.ItemArray[33].ToString() },
                         { codibook.Columns[34].ColumnName, row.ItemArray[34].ToString() },
-                        { codibook.Columns[35].ColumnName, row.ItemArray[35].ToString() },
-                        { codibook.Columns[36].ColumnName, row.ItemArray[36].ToString() },
                     });
                 }
                 catch (Exception)
                 {
+                    // Show the result of skipped students 
+                    var studentnumber = row.ItemArray[0].ToString();
+                    var advice = row.ItemArray[6].ToString();
+                    var conclusion = "(Twijfel)";
+                    table.AddRow(studentnumber, advice, conclusion);
+
                     continue;
                 }
 
@@ -171,9 +139,23 @@ namespace DecisionTree
                     string answer = codibook.Revert("advies", predicted);
 
                     // Show the result in the output
-                    Console.WriteLine("Conclusion " + row.ItemArray[0].ToString() + ": " + answer);
+                    var studentnumber = row.ItemArray[0].ToString();
+                    var advice = row.ItemArray[6].ToString();
+                    var conclusion = answer;
+                    table.AddRow(studentnumber, advice, conclusion);
+                }
+                else
+                {
+                    // Show the result of skipped students  
+                    var studentnumber = row.ItemArray[0].ToString();
+                    var advice = row.ItemArray[6].ToString();
+                    var conclusion = "(Twijfel)";
+                    table.AddRow(studentnumber, advice, conclusion);
                 }
             }
+
+            // Write the table in console
+            table.Write();
 
             // Read Key
             Console.ReadKey();
